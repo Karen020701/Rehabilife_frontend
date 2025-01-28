54d3e7cbad32701b3fe685d4c660145856bbc002
Karen020701 <karenchicaiza989@gmail.com>
2025-01-27 21:54:13 -0500

    feat: :sparkles: Updating microservice ports in URLs

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -2,11 +2,12 @@ const config = {
   BASE_URL_AUTH: 'http://localhost:3001/api/auth/login',
   BASE_URL_REGISTER:'http://localhost:3002/api/users',
   BASE_URL_OFFERS: 'http://localhost:8080/index.php',
-  BASE_URL_PRODUCT_CREATE: 'http://localhost:3012/api/products',
-  BASE_URL_PRODUCT_DELETE: 'http://localhost:3013/deleteProduct',
-  BASE_URL_PRODUCT_UPDATE: 'http://localhost:3014/api/products',
-  BASE_URL_PRODUCT: 'http://localhost:3015/api/products',
+  BASE_URL_PRODUCT_CREATE: 'http://localhost:3006/api/products',
+  BASE_URL_PRODUCT_DELETE: 'http://localhost:3009/deleteProduct',
+  BASE_URL_PRODUCT_UPDATE: 'http://localhost:3007/api/products',
+  BASE_URL_PRODUCT: 'http://localhost:3008/api/products',
   
 };
 
 export default config;
+
