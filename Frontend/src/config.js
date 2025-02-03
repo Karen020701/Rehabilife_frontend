14b686d6dab3cff875ba30dba6080ff2c73cd047
Alejandra Mishell Calvache Ortiz <amcalvacheo@uce.edu.ec>
2025-02-03 18:27:05 -0500

    Delete Frontend/src directory

--- Frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  BASE_URL_AUTH: process.env.REACT_APP_BASE_URL_AUTH,
-  BASE_URL_REGISTER: process.env.REACT_APP_BASE_URL_REGISTER,
-  BASE_URL_OFFERS: process.env.REACT_APP_BASE_URL_OFFERS,
-};
-
-export default config;
